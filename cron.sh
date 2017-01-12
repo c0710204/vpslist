b3a3b93fa9ddf901b2cdd6fbb6978671026ddf9f
Xiang Gu <c0710204@gmail.com>
2017-01-12 19:10:36 +0000

    autosync

--- cron.sh
+++ cron.sh
@@ -6,7 +6,7 @@ ip=`curl -s http://whatismyip.akamai.com/`
 mem=`free -h|grep Mem|awk "{print \\\$2}"`
 cpu=`cat /proc/cpuinfo |grep processor|wc -l`
 git pull
-rm "$host_\*"
+rm "$host\*"
 touch "[$host][$ip][$cpu][$mem]"
 git add . --all
 git commit -m autosync
